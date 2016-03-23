65a723ce17c903db11f24ef5b3297b751b34bb9d
刘泽祥 <liuzexiang@live.cn>
2016-03-23 11:07:07 +0800

    FirstCommit
    NSArray和NSDictionary

---
+++ NSArray+LZXLog.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+LZXLog.h
+//  getkey
+//
+//  Created by 刘泽祥 on 15/8/7.
+//  Copyright (c) 2015年 刘泽祥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Log)
+
+@end
+
+@interface NSDictionary (Log)
+
+@end
---
+++ NSArray+LZXLog.m
@@ -0,0 +1,43 @@
+//
+//  NSArray+LZXLog.m
+//  getkey
+//
+//  Created by 刘泽祥 on 15/8/7.
+//  Copyright (c) 2015年 刘泽祥. All rights reserved.
+//
+
+#import "NSArray+LZXLog.h"
+
+@implementation NSArray (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }];
+    
+    [strM appendString:@")"];
+    
+    return strM.copy;
+}
+
+@end
+
+@implementation NSDictionary (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    
+    [strM appendString:@"}\n"];
+    
+    return strM.copy;
+}
+
+@end
